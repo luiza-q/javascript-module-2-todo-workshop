90d7933ec30d9ddd8a473eb75492c417dd890fa0
Luiza Quaglio <luizaq@gmail.com>
2021-08-04 20:07:01 +0200

    exercises 11 to 15 - Inclass

--- scripts/script.js
+++ scripts/script.js
@@ -13,18 +13,62 @@ el.addEventListener('submit', (e) => {
 
 const todos =  [];
 
-const createTodo = (text) => {
-    todos.push(text);
+const filters = {
+    searchTitle: '',
+    showFinished: false,
+    showUnfinished: false
 }
 
-const generateTodoDOM = (todo) => {
+const setFilters = (updates) => {
+    if (typeof updates.searchTitle === 'string') {
+       filters.searchTitle = updates.searchTitle;
+    }
+
+    if (typeof updates.showFinished === 'boolean') {
+        filters.showFinished = updates.showFinished;
+    }
+
+    if (typeof updates.showUnfinished === 'boolean') {
+        filters.showUnfinished = updates.showUnfinished;
+    }
+}
+
+
+
+
+
+
+
+
+
+const createTodo = (text) => {
+    todos.push({
+        title: text,
+        completed: false
+    })
+}
+
+const generateTodoDOM = (todoObj) => {
     const todoEl = document.createElement('label');
     const containerEl = document.createElement('div');
     const todoText = document.createElement('span');
+    const checkbox = document.createElement('input');
+
+    // Setup checkbox
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.checked = todoObj.completed;
+    checkbox.addEventListener('change', () => {
+        toggleToDo(todoObj.title);
+        renderTodos(todos);
+        }
+    );
+
 
     // Setup the todo text
-    todoText.textContent = todo;
+    todoText.textContent = todoObj.title;
+    containerEl.appendChild(checkbox);
     containerEl.appendChild(todoText);
+    
 
     // Setup container
     todoEl.classList.add('list-item');
@@ -37,11 +81,20 @@ const generateTodoDOM = (todo) => {
     removeButton.classList.add('button', 'button--text');
     todoEl.appendChild(removeButton);
     removeButton.addEventListener('click', () => {
-        removeTodo(todoText)
+        removeTodo(todoObj.title)
         renderTodos(todos)
     })
 
     return todoEl;
+}
+
+const toggleToDo = (title) => {
+    const todo = todos.find(todo => todo.title === title);
+
+    if (todo) {
+        todo.completed = !todo.completed
+    }
+
 }
 
 const renderTodos = (todos) => {
@@ -60,9 +113,9 @@ const renderTodos = (todos) => {
     }
 }
 
-const removeTodo = (todoEl) => {
+const removeTodo = (title) => {
     const todoIndex = todos.findIndex((todo) => {
-        return todo === todoEl.textContent;
+        return todo.title === title;
     })
     if (todoIndex > -1) {
         todos.splice(todoIndex, 1);
